e00df7ad0fc4ee58e132bb66cd36ba92a0804809
tr0f1m0v <killerlaod@gmail.com>
2021-06-10 14:12:12 +0300

    Статистика изображения

---
+++ lab8sppz.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('C://Users/ozero.jpg')
+color = ('b','g','r')
+
+for i,col in enumerate(color):
+    histr = cv.calcHist([img],[i],None,[256],[0,256])
+    plt.plot(histr,color = col)
+    plt.xlim([0, 256])
+
+plt.show()
